ae4afc27c5a4f8855933211c1cbae93ac726bb25
vive-card <TorselloDanilo1994@gmail.com>
2026-03-31 18:42:06 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { RootNavigator } from "./src/navigation/RootNavigator";
+import RootNavigator from "./src/navigation/RootNavigator";
 
 export default function App() {
   return (
