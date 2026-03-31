8e24a1639f84fa780b56d431b34a5a7b3fb506ae
vive-card <TorselloDanilo1994@gmail.com>
2026-03-31 17:58:26 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,20 +2,28 @@ import "react-native-url-polyfill/auto";
 import * as SecureStore from "expo-secure-store";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || "";
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "";
 
-const ExpoSecureStoreAdapter = {
-  getItem: (key: string) => SecureStore.getItemAsync(key),
-  setItem: (key: string, value: string) => SecureStore.setItemAsync(key, value),
-  removeItem: (key: string) => SecureStore.deleteItemAsync(key)
-};
+class ExpoSecureStoreAdapter {
+  getItem(key: string) {
+    return SecureStore.getItemAsync(key);
+  }
+
+  setItem(key: string, value: string) {
+    return SecureStore.setItemAsync(key, value);
+  }
+
+  removeItem(key: string) {
+    return SecureStore.deleteItemAsync(key);
+  }
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    persistSession: true,
+    storage: new ExpoSecureStoreAdapter(),
     autoRefreshToken: true,
-    detectSessionInUrl: false,
-    storage: ExpoSecureStoreAdapter
+    persistSession: true,
+    detectSessionInUrl: false
   }
 });
