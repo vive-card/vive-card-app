0847908a80a1234cecf53f05f9ce731a91519d50
vive-card <TorselloDanilo1994@gmail.com>
2026-03-31 15:05:48 +0200

    Update AppNavigator.tsx

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -4,11 +4,22 @@ import { DashboardScreen } from "../screens/DashboardScreen";
 import { CardScreen } from "../screens/CardScreen";
 import { SettingsScreen } from "../screens/SettingsScreen";
 
-const Tab = createBottomTabNavigator();
+export type AppTabParamList = {
+  Dashboard: undefined;
+  Karte: undefined;
+  Settings: undefined;
+};
+
+const Tab = createBottomTabNavigator<AppTabParamList>();
 
 export function AppNavigator() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator
+      initialRouteName="Dashboard"
+      screenOptions={{
+        headerShown: false
+      }}
+    >
       <Tab.Screen name="Dashboard" component={DashboardScreen} />
       <Tab.Screen name="Karte" component={CardScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
